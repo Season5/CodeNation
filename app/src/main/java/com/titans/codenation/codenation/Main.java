bb94161c619eb77c3ecd4b6611efd2678064a664
joseph muli <jayjaymuli00@gmail.com>
2016-03-16 07:30:20 +0300

    views added

--- app/src/main/java/com/titans/codenation/codenation/Main.java
+++ app/src/main/java/com/titans/codenation/codenation/Main.java
@@ -1,8 +1,11 @@
 package com.titans.codenation.codenation;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.widget.Toolbar;
 
@@ -28,12 +31,25 @@ public class Main extends AppCompatActivity implements NavigationView.OnNavigati
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
 
-        int id == item.getItemId();
+        int id = item.getItemId();
 
         if(id == R.id.nav_camera){
             //handle camera output...navigation
+        }else if(id == R.id.nav_gallery){
+            //handle gallery input
+        }else if(id == R.id.nav_slideshow){
+            //handle output
+        }else if(id == R.id.nav_manage){
+            //handle output
+        }else if(id == R.id.nav_share){
+            //output
+        }else if(id == R.id.nav_send){
+
         }
 
-        return false;
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
